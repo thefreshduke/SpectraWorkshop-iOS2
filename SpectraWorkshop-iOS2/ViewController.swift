5ca3f045aec3b8d3f54aed74f5fc0aa85d43b2e9
thefreshduke <scottyshaw1@gmail.com>
2016-07-22 12:01:27 -0700

    Modified stopwatch from SpectraWorkshop-iOS1 to create pulseButton()

--- SpectraWorkshop-iOS2/ViewController.swift
+++ SpectraWorkshop-iOS2/ViewController.swift
@@ -9,16 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var myButton: UIButton!
+    
+    var counter: Double = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let repeatingFunction: Selector = #selector(ViewController.pulseButton)
+        _ = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: repeatingFunction, userInfo: nil, repeats: true)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func pulseButton() {
+        counter += 1.0
+        
+        // calculate elapsed time
+        let adjustmentFactor = (counter / 100.0) % 10.0
+        var sizeAdjustment: Double = 0.0
+        var alphaAdjustment: Double = 0.0
+        
+        // increase size and alpha
+        if (adjustmentFactor < 5) {
+            sizeAdjustment = (80.0 + 4.0 * adjustmentFactor) / 100.0
+            alphaAdjustment = (40.0 + 12.0 * adjustmentFactor) / 100.0
+        }
+        // decrease size and alpha
+        else {
+            sizeAdjustment = (120.0 - 4.0 * adjustmentFactor) / 100.0
+            alphaAdjustment = (160.0 - 12.0 * adjustmentFactor) / 100.0
+        }
+        
+        myButton.titleLabel!.font =  UIFont(name: (myButton.titleLabel?.font?.fontName)!, size: 75.0 * CGFloat(sizeAdjustment))
+        myButton.titleLabel!.alpha = CGFloat(alphaAdjustment)
+    }
+    
 }
 
