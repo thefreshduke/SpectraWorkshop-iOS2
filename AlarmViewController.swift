57bea7460fb50b5d258347eb87d40fbde5dff322
thefreshduke <scottyshaw1@gmail.com>
2016-07-22 17:05:13 -0700

    Added alarm song on loop

--- AlarmViewController.swift
+++ AlarmViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import AVFoundation
 
 class AlarmViewController: UIViewController {
+    
+    var alarmSong: AVAudioPlayer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if let dosEquisThemeSong = self.setupAudioPlayerWithFile("DosEquisThemeSong", type:"mp3") {
+            self.alarmSong = dosEquisThemeSong
+        }
+        alarmSong?.numberOfLoops = -1
+        alarmSong?.volume = 1
+        alarmSong?.play()
     }
     
     override func didReceiveMemoryWarning() {
@@ -20,4 +30,24 @@ class AlarmViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func setupAudioPlayerWithFile(file: NSString, type: NSString) -> AVAudioPlayer?  {
+        
+        // Step 1
+        let path = NSBundle.mainBundle().pathForResource(file as String, ofType: type as String)
+        let url = NSURL.fileURLWithPath(path!)
+        
+        // Step 2
+        var audioPlayer: AVAudioPlayer?
+        
+        // Step 3
+        do {
+            try audioPlayer = AVAudioPlayer(contentsOfURL: url)
+        }
+        catch {
+            print("Player not available")
+        }
+        
+        // Step 4
+        return audioPlayer
+    }
 }
