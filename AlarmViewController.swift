b62c1925545ceac38d150a5cbf2ee485c99d2cd9
thefreshduke <scottyshaw1@gmail.com>
2016-07-23 09:26:58 -0700

    Added Twitter log-in functionality

--- AlarmViewController.swift
+++ AlarmViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import AVFoundation
+import TwitterKit
 
 class AlarmViewController: UIViewController {
     
@@ -23,6 +24,23 @@ class AlarmViewController: UIViewController {
         alarmSong?.numberOfLoops = -1
         alarmSong?.volume = 1
         alarmSong?.play()
+        
+        let logInButton = TWTRLogInButton { (session, error) in
+            if let unwrappedSession = session {
+                let alert = UIAlertController(title: "Logged In",
+                    message: "User \(unwrappedSession.userName) has logged in",
+                    preferredStyle: UIAlertControllerStyle.Alert
+                )
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+                self.presentViewController(alert, animated: true, completion: nil)
+            } else {
+                NSLog("Login error: %@", error!.localizedDescription);
+            }
+        }
+        
+        // TODO: Change where the log in button is positioned in your view
+        logInButton.center = self.view.center
+        self.view.addSubview(logInButton)
     }
     
     override func didReceiveMemoryWarning() {
